c3fb41c289c3f768afdf386d39ec82379930c7c7
bharath2408 <bharatha24kumar@gmail.com>
2022-03-25 20:52:19 +0530

    basics

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('Your code goes here');
+const person ={
+    name: 'bharathakumar',
+    age: 23
+};
+
+console.log(person.name);
+
+// ITS NOT FOR GOOD PRACTICE 
+// const person: {
+//     name: string;
+//     age: number;
+// } ={
+//     name: 'bharathakumar',
+//     age: 23
+// };
+
+// console.log(person.name);
---
+++ basics.ts
@@ -0,0 +1,17 @@
+function add(n1: number, n2: number, showResult: boolean, pharse: string){
+    
+    const result = n1 + n2;
+    if(showResult){
+        console.log(pharse + result);
+    }else{
+
+        return result;
+    }
+}
+
+const number1 = 5;
+const number2 = 2.5;
+const printResult = true;
+const resultPharse = 'Result is : '
+
+add(number1, number2, printResult, resultPharse);
