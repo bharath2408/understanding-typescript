907205845dbd45bdfae6b5cdd46bd9035e0d4256
bharath2408 <bharatha24kumar@gmail.com>
2022-03-26 17:14:10 +0530

    array

--- app.ts
+++ app.ts
@@ -1,17 +1,35 @@
-const person ={
-    name: 'bharathakumar',
-    age: 23
-};
-
-console.log(person.name);
-
 // ITS NOT FOR GOOD PRACTICE 
 // const person: {
-//     name: string;
-//     age: number;
-// } ={
-//     name: 'bharathakumar',
-//     age: 23
-// };
+    //     name: string;
+    //     age: number;
+    // } ={
+        //     name: 'bharathakumar',
+        //     age: 23
+        // };
+        
+        // console.log(person.name);
 
-// console.log(person.name);
+const person ={
+    name: 'bharathakumar',
+    age: 23,
+    hobbies: ['Sports', 'Cooking']
+
+    };
+
+console.log(person);
+    
+for (const hobby of person.hobbies){
+    console.log(hobby);
+}
+
+// String of array    
+let favoriteActivities: string[] ;
+favoriteActivities = ['Sports']
+        
+console.log(favoriteActivities);
+        
+//Array of numbers
+let favoriteNumbers: number[] ;
+favoriteNumbers = [1,2,3]
+        
+ console.log(favoriteNumbers);
